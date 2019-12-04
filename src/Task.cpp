d874b72a3078d0408ebb99784a1cb4c7fb0034d4
xiaoyi <781210678@qq.com>
2019-12-05 01:27:24 +0800

    linxiaoyi_1205

--- src/Task.cpp
+++ src/Task.cpp
@@ -49,3 +49,130 @@ int main()
 
 
 
+
+
+
+/*
+小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个，小A 每次也从 1, 2, 3 中选择一个猜。他们一共进行三次这个游戏，请返回 小A 猜对了几次？
+
+输入的guess数组为 小A 每次的猜测，answer数组为 小B 每次的选择。guess和answer的长度都等于3。
+
+示例 1：
+输入：guess = [1,2,3], answer = [1,2,3]
+输出：3
+解释：小A 每次都猜对了。
+
+示例 2：
+输入：guess = [2,2,3], answer = [3,2,1]
+输出：1
+解释：小A 只猜对了第二次。
+
+限制：
+
+guess的长度 = 3
+answer的长度 = 3
+guess的元素取值为 {1, 2, 3} 之一。
+answer的元素取值为 {1, 2, 3} 之一。
+*/
+// task2
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int game(vector<int>& guess, vector<int>& answer) {
+        int times = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if(guess[i] == answer[i])
+            {
+                times++;
+            }
+        }
+        return times;
+    }
+};
+int main()
+{
+    vector<int> gusess;
+    vector<int> answer;
+    int temp;
+    for(int i = 0; i < 3; i++)
+    {
+        cin >> temp;
+        gusess.push_back(temp);
+    }
+
+    for(int i = 0; i < 3; ++i)
+    {
+        cin >> temp;
+        answer.push_back(temp);
+    }
+
+    Solution sl;
+    int times = sl.game(gusess, answer);
+    cout << "right times is " << times << endl;
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+ 给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+示例 1:
+输入: J = "aA", S = "aAAbbbb"
+输出: 3
+
+示例 2:
+输入: J = "z", S = "ZZ"
+输出: 0
+*/
+
+
+
+//task3
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        int count = 0;
+        for(int i =0; i < J.size(); i++)
+        {
+            for(int j = 0 ; j < S.size(); j++)
+            {
+                if(J[i] == S[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    std::string J,S;
+    std::cout << "qing shu ru ni de bao shi lei xing: ";
+    getline(std::cin, J);
+    std::cout << "qing shu ru ni de shi tou lei xing: ";
+    getline(std::cin,S);
+    Solution sl;
+    int count = sl.numJewelsInStones(J,S);
+    std::cout << "numers Jevels in stones is : " << count << std::endl;
+    return 0;
+}
+*/
