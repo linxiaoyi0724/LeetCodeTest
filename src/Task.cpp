e5b2f2795f7eaf78f554ec32cf265d1a98b61425
xiaoyi <781210678@qq.com>
2019-12-12 02:09:37 +0800

    linxiaoyi_1212

--- src/Task.cpp
+++ src/Task.cpp
@@ -306,3 +306,129 @@ int main()
 
 
 
+
+
+
+/*
+//task 6
+给你一个整数 n，请你帮忙计算并返回该整数「各位数字之积」与「各位数字之和」的差。
+
+示例 1：
+输入：n = 234
+输出：15 
+解释：
+各位数之积 = 2 * 3 * 4 = 24 
+各位数之和 = 2 + 3 + 4 = 9 
+结果 = 24 - 9 = 15
+
+示例 2：
+输入：n = 4421
+输出：21
+解释： 
+各位数之积 = 4 * 4 * 2 * 1 = 32 
+各位数之和 = 4 + 4 + 2 + 1 = 11 
+结果 = 32 - 11 = 21
+*/
+
+
+/*
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        while ( n != 0)
+        {
+            int remainder = n % 10;
+            product *= remainder;
+            sum += remainder;
+            n = n / 10;
+        }
+        return product - sum; 
+    }
+};
+
+#include <iostream>
+int main()
+{
+    int data;
+    std::cout << "Please input your data: ";
+    std::cin>>data;
+    Solution sl;
+    int result = sl.subtractProductAndSum(data);
+    std::cout << "result: " << result<<std::endl;
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+//task 7
+
+平面上有 n 个点，点的位置用整数坐标表示 points[i] = [xi, yi]。请你计算访问所有这些点需要的最小时间（以秒为单位）。
+你可以按照下面的规则在平面上移动：
+每一秒沿水平或者竖直方向移动一个单位长度，或者跨过对角线（可以看作在一秒内向水平和竖直方向各移动一个单位长度）。
+必须按照数组中出现的顺序来访问这些点。
+ 
+
+示例 1：
+输入：points = [[1,1],[3,4],[-1,0]]
+输出：7
+解释：一条最佳的访问路径是： [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]   
+从 [1,1] 到 [3,4] 需要 3 秒 
+从 [3,4] 到 [-1,0] 需要 4 秒
+一共需要 7 秒
+
+示例 2：
+输入：points = [[3,2],[-2,2]]
+输出：5
+*/
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int minTime = 0;
+        for(int i = 0; i < points.size()-1;i++)
+        {
+            int minX = abs(points[i][0] - points[i+1][0]);
+            int minY = abs(points[i][1] - points[i+1][1]);
+            minTime += minX>minY?minX:minY;
+        }
+        return minTime;
+    }
+};
+int main()
+{
+    
+    int numPoints;
+    cout << "How many points do you want to input? ";
+    cin >>numPoints;
+    vector<vector<int>> points(numPoints,vector<int>(2));
+    for(int i = 0; i < numPoints; i++)
+    {
+        for(int j = 0; j < 2; j++)
+        {
+            cin>>points[i][j];
+        }
+    }
+    Solution sl;
+    int minTimes = sl.minTimeToVisitAllPoints(points);
+    std::cout << "Min times: "<<minTimes <<endl;
+    return 0;
+}
+*/
+
+
+
