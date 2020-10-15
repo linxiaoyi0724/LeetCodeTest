d23da4bf77e78879702e4b31099b5d84732b4db7
xiaoyi <linyihuang@hitech.com>
2020-10-15 18:22:17 +0800

    linxiaoyi_20201015

--- src/Task.cpp
+++ src/Task.cpp
@@ -1089,3 +1089,59 @@ int main()
 
 
 
+// Task 17
+/*
+在老式手机上，用户通过数字键盘输入，手机将提供与这些数字相匹配的单词列表。每个数字映射到0至4个字母。给定一个数字序列，实现一个算法来返回匹配单词的列表。你会得到一张含有有效单词的列表。映射如下图所示：
+
+
+
+示例 1:
+
+输入: num = "8733", words = ["tree", "used"]
+	输出 : ["tree", "used"]
+	示例 2 :
+
+	输入 : num = "2", words = ["a", "b", "c", "d"]
+	输出 : ["a", "b", "c"]
+	提示：
+
+	num.length <= 1000
+	words.length <= 500
+	words[i].length == num.length
+	num中不会出现 0, 1 这两个数字
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+	vector<string> getValidT9Words(string num, vector<string>& words) {
+
+	}
+};
+
+int main()
+{
+	string numberInput;
+	vector<string> wordsInput;
+	string wordTemp;
+
+	cout << "Please input string number:";
+	cin >> numberInput;
+	cout << "Please input string ";
+	for (int i = 0; i < stoi(numberInput); i++)
+	{
+		cin >> wordTemp;
+		wordsInput.emplace_back(wordTemp);
+		cout << "Please input string ";
+	}
+
+	cout << numberInput << endl;;
+	for (int i = 0; i < stoi(numberInput); i++)
+		cout << wordsInput[i] << endl;
+
+	int xiaoyi;
+}
